925113d6c5a412621a147008cfa2f20795a97afe
Gustavo <gustavo.lovera@gmail.com>
2023-11-14 15:56:36 -0300

    SegundoCommit

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const expres = require('express')
+const express = require('express')
+//Ejecutar un midewer es decir funcion que se ejecuta antes de ingresar a cada ruta
+const multer = require('multer')
+const upload = multer({dest: 'uploads/'})
 
-const app =  expres()
+const app =  express()
+
+app.use(express.json())
 
 app.get('/', function(req, res){
-    res.send('Hola mundo!!')
-
+    
+    res.send('Hola mundo!!!!')
 })
+
+app.post('/imagen', upload.single('imagen'), function(req, res){
+    const body = req.body
+    const imagen = req.file
+
+    console.log(imagen)
+    res.send('Hola mundo desde el POST')
+})
+
 app.listen(3000)
 
 /* Declarar funciones
